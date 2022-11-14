0e49c5c887a6df2eb4a6770b88c4e6688b29bfb6
Max <maks.zmp@yandex.ru>
2022-11-14 14:10:25 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once "connect.php";
+echo "<pre>";
+
+if (($file = fopen('TestFile.csv', 'r')) !== false) {
+    $conn = dbConn();
+    while (($fileData = fgetcsv($file, 5000, ';')) !== false) {
+        $find = "SELECT * FROM `mittora_test` WHERE `goods` = '$fileData[1]'";
+
+        $data = $conn->prepare($find);
+        $data->execute();
+
+        $count = $data->rowCount();
+        echo $count . " ";
+
+        if (!$count) {
+            $insert_query = "INSERT INTO `mittora_test`( `chapter`, `goods`, `count`) VALUES ('$fileData[0]','$fileData[1]','$fileData[2]')";
+            // $conn = dbConn();
+            $data = $conn->prepare($insert_query);
+            $data->execute();
+        }
+    }
+    fclose($file);
+    $conn = null;
+}
